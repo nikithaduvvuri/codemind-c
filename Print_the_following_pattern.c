43f71838ff3fc7b7e57032c4990957c90e9121d6
nikithaduvvuri <97437377+nikithaduvvuri@users.noreply.github.com>
2022-03-09 15:44:28 +0530

    #include<stdio.h> int main() {     int a,n,i,j=1,count=1;     scanf("%d",&a);     for(i=a;i>0;i--)     {         while(j<=a)         {             printf("%d ",j);             j++;         }         count++;         j=count;         printf(" ");     }     return 0; }

---
+++ Print_the_following_pattern.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,n,i,j=1,count=1;
+    scanf("%d",&a);
+    for(i=a;i>0;i--)
+    {
+        while(j<=a)
+        {
+            printf("%d ",j);
+            j++;
+        }
+        count++;
+        j=count;
+        printf("
+");
+    }
+    return 0;
+}
