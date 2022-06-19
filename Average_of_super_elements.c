bd6b14f13a37e53b502732ea0033333451d96674
nikithaduvvuri <97437377+nikithaduvvuri@users.noreply.github.com>
2022-06-19 19:40:45 +0530

    #include<stdio.h> int main() {     int n,a[100],c=0,f=0;     float sum=0,avg;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     {c=0;       for(int j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c==a[i])         {             sum=sum+a[i];             f++;             a[i]=0;         }     }     if(f==0)     {         printf("-1");     }     else     {         avg=float(sum)/f;         printf("%.2f",avg);     }
    }

--- Average_of_super_elements.c
+++ Average_of_super_elements.c
@@ -1,35 +1,37 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],k=0;
+    int n,a[100],c=0,f=0;
     float sum=0,avg;
     scanf("%d",&n);
-    for(int i=00;i<n;i++)
+    for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     for(int i=0;i<n;i++)
-    {int c=0;
-         for(int j=0;j<n;j++)
-         {
-             if(a[i]==a[j])
-             {
-                 c++;
-             }
-         }
-         if(c==a[i])
-         {
-             sum=sum+a[i];
-             a[i]=0;
-             k++;
-         }
-        
+    {c=0;
+      for(int j=0;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(c==a[i])
+        {
+            sum=sum+a[i];
+            f++;
+            a[i]=0;
+        }
     }
-    if(k==0)
-    printf("-1");
+    if(f==0)
+    {
+        printf("-1");
+    }
     else
     {
-        avg=float(sum)/k;
+        avg=float(sum)/f;
         printf("%.2f",avg);
     }
-}    
+    
+}
