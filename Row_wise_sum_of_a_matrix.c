b4a6bb5c986fe05ce63ff8db3b1261b7207ea20f
nikithaduvvuri <97437377+nikithaduvvuri@users.noreply.github.com>
2022-07-20 21:44:03 +0530

    #include<stdio.h> int main() {     int n,m,a[100][100],max=0,s=0;     scanf("%d %d",&n,&m);     for(int i=0;i<n;i++)     {  s=0;         for(int j=0;j<m;j++)         {             scanf("%d",&a[i][j]);             s=s+a[i][j];         }         printf("%d ",s);     } }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,a[100][100],max=0,s=0;
+    scanf("%d %d",&n,&m);
+    for(int i=0;i<n;i++)
+    {  s=0;
+        for(int j=0;j<m;j++)
+        {
+            scanf("%d",&a[i][j]);
+            s=s+a[i][j];
+        }
+        printf("%d ",s);
+    }
+}    
